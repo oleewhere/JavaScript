a050eaa2e89bd194643fedaff5b1af41dfa97428
fl4p1k <olegischenko06@gmail.com>
2026-07-29 19:56:34 +0200

    Changed directories

---
+++ Homeworks/HW7/HW7_1/script.js
@@ -0,0 +1,29 @@
+//Власний ForEach
+Array.prototype.ownForEach = function(callback, thisArg) {
+    for (let i = 0; i < this.length; i++) {
+        if (i in this){
+            callback.call(thisArg, this[i], i, this);
+        }
+    }
+}
+let numbers = [1, 2, 3, 4, 11, 12, 16, 22];
+numbers.ownForEach((item,id) => {
+    console.log(`${item}, id: ${id}`)
+})
+
+
+//Власний Filter
+Array.prototype.ownFilter = function(callback, thisArg) {
+const result = [];
+    for (let i = 0; i < this.length; i++) {
+        if (i in this){
+            if(callback.call(thisArg, this[i], i, this)) {
+                result.push(this[i]);
+            }
+        }
+    }
+    return result;
+}
+let nums = [1, 3, 4, 11, 12, 16, 17, 20, 22];
+const oddNumbers = nums.ownFilter(num => num % 2 === 1);
+console.log(oddNumbers);
